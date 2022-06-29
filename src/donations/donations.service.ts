7d59bf77b9e3e27af396f1b5b091a340eb8e5f83
Stephanie <stephaniiehallberg@gmail.com>
2022-06-29 16:03:59 +0200

    feat(donations): findAll prisma

--- src/donations/donations.service.ts
+++ src/donations/donations.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDonationInput } from './dto/create-donation.input';
+import { PrismaService } from './../../prisma/prisma.service';
 
 @Injectable()
 export class DonationsService {
+  constructor(private prisma: PrismaService) {}
   create(createDonationInput: CreateDonationInput) {
     return 'This action adds a new donation';
   }
 
   findAll() {
-    return [
-      {
-        exampleField: 1,
-      },
-    ];
+    return this.prisma.donation.findMany();
   }
 
   findOne(id: number) {
