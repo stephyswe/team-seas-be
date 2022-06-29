952ccdb653108a63128249893d6258577aac6549
Stephanie <stephaniiehallberg@gmail.com>
2022-06-29 14:05:31 +0200

    feat(resource): donations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+      typePaths: ['./**/*.graphql'],
+    }),
+    DonationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
