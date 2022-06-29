1b7dbbb8e641b343d96cdacbb86b169a0ce0a2b1
Stephanie <stephaniiehallberg@gmail.com>
2022-06-29 15:20:34 +0200

    feat(prisma): sqlite db & seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLISODateTime, GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { GraphQLDateTime } from 'graphql-iso-date';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DonationsModule } from './donations/donations.module';
@@ -15,11 +13,9 @@ import { DonationsModule } from './donations/donations.module';
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault],
       typePaths: ['./**/*.graphql'],
-      resolvers: { DateTime: GraphQLDateTime },
+      resolvers: { DateTime: GraphQLISODateTime },
     }),
     DonationsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
